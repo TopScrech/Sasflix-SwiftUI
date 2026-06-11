7cf8bad683b35778685b3ae0745bdda60f7c0b2e
TopScrech <sergei_saliukov@icloud.com>
2026-06-11 02:17:01 +0200

    removed Solovev Live video tag from release

--- Sasflix/Views/MetadataBadgeView.swift
+++ Sasflix/Views/MetadataBadgeView.swift
@@ -5,8 +5,18 @@ struct MetadataBadgeView: View {
     let systemImage: String
     
     var body: some View {
+#if DEBUG
         Label(title, systemImage: systemImage)
             .caption()
             .secondary()
+#else
+        if item.category == .solovev {
+            EmptyView()
+        } else {
+            Label(title, systemImage: systemImage)
+                .caption()
+                .secondary()
+        }
+#endif
     }
 }
