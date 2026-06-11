01256a4dfeee8cc8b617cea40fbd5a89acd1e49c
TopScrech <sergei_saliukov@icloud.com>
2026-06-11 16:20:37 +0200

    removed tech videos from release

--- Sasflix/Models/FeedCategory.swift
+++ Sasflix/Models/FeedCategory.swift
@@ -60,7 +60,7 @@ nonisolated enum FeedCategory: String, CaseIterable, Identifiable, Codable, Send
 #if DEBUG
         FeedCategory.allCases
 #else
-        [.all, .topics, .commentator, .trainings, .critic, .documentary, .review, .techno, .streams, .algoritm]
+        [.all, .topics, .commentator, .trainings, .critic, .documentary, .review, .streams, .algoritm]
 #endif
     }
 }
