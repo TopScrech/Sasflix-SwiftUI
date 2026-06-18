332e1411b1c2653d7b1fd27961be59cb8caebed0
TopScrech <sergei_saliukov@icloud.com>
2026-06-18 19:55:00 +0200

    Added github link

--- Sasflix/Views/Settings/AccountSettings.swift
+++ Sasflix/Views/Settings/AccountSettings.swift
@@ -91,6 +91,23 @@ struct AccountSettings: View {
                 Button("Открыть RSS", systemImage: "dot.radiowaves.left.and.right", action: openRSS)
                 Button("Поддержка", systemImage: "envelope", action: openSupport)
             }
+            
+            Section {
+                Link(destination: URL(string: "https://github.com/TopScrech/Sasflix-SwiftUI")!) {
+                    HStack(spacing: 12) {
+                        Image(.gitHub)
+                            .resizable()
+                            .frame(24)
+                            .clipShape(.circle)
+                        
+                        Text("GitHub")
+                    }
+                }
+                .tint(.primary)
+            } footer: {
+                Text("Bug reports, feature requests & contributions are always welcome!")
+                    .secondary()
+            }
         }
         .navigationTitle("Аккаунт")
         .overlay {
